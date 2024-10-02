5fb929f1461c8d89e0c03e068212b444059b9e8d
Peter King <30704554+snowbrdking26@users.noreply.github.com>
2024-10-02 08:55:28 -0400

    updated index

---
+++ index.ts
@@ -0,0 +1,23 @@
+import express, {Request, Response} from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/test", async (req: Request, res: Response)=>{
+ res.json({ message: "Hello!" });
+});
+
+// localhost
+app.listen(7000, ()=>{
+    console.log("server started on localhost:7000");
+});
+
+app.get("/equipment", async (req: Request, res: Response)=>{
+ res.json({ message: "Equipment Resupply list!" });
+});
+
+
+
